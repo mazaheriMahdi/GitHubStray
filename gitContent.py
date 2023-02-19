8fbea0e0ab28be3b3fcfbd91dc30c5c8588dd705
mazaheriMahdi <mahdi.mazaheri8383@gmail.com>
2023-02-19 18:09:51 +0330

    fix git content class

--- gitContent.py
+++ gitContent.py
@@ -3,7 +3,8 @@ import rumps
 
 class GitContentMeue(rumps.MenuItem):
     def __init__(self, title, type, date):
-        super().__init__(title)
+        super().__init__(title + "\t\t" + date)
         self.type = type
         self.date = date
+
 
